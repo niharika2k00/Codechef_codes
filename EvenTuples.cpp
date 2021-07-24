e53c4dfa0c2e4624943382ce5e718a143020bba1
niharika2k00 <dniharika16@gmail.com>
2021-07-24 23:47:51 +0530

    24/07/2021

--- EvenTuples.cpp
+++ EvenTuples.cpp
@@ -1,15 +1,13 @@
 
-//LINKS ::
+//https://www.codechef.com/START6C/problems/ETUP
+// STARTERS ---> 2021 (JULY)
 
 #include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-#include <cmath>
 using namespace std;
 
 #define ll long long
-#define vi vector<int>
 #define vll vector<ll>
+#define vi vector<int>
 #define pb push_back
 #define mp make_pair
 #define loop1(n) for (ll i = 0; i < (n); i++)
@@ -24,6 +22,18 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+    test
+    {
+        ll n, q;
+        cin >> n >> q;
+        vi arr(n), Left, Right;
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        for (int i = 0; i < q; i++)
+            cin >> Left[i] >> Right[i];
+    }
 
     return 0;
 }
