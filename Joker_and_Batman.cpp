bce94d08a400cce76eea9dbd5c51583097adaa04
niharika2k00 <dniharika16@gmail.com>
2021-07-25 16:59:48 +0530

    tuples

--- Joker_and_Batman.cpp
+++ Joker_and_Batman.cpp
@@ -1,6 +1,7 @@
 
 //           https://www.codechef.com/START6C/problems/JOKRBTMN
 //              STARTERS ---> 2021 (JULY)
+//                IMPORTANT GOOOD PROGRAMMM
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -23,8 +24,39 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
+    /* 
+N -->  number of colors, 
+M -->  number of lists,
+L --> length of the strip,
+ */
     test
     {
+        int i, j, n, m, l, k, id, ans = 0;
+        map<int, int> map;
+
+        cin >> n >> m >> l;
+        vector<int> strip(l);
+
+        for (i = 0; i < m; i++) // m lists
+        {
+            cin >> k; // length of each list
+            for (j = 0; j < k; j++)
+            {
+                cin >> id;
+                map[id] = i;
+            }
+        }
+
+        for (i = 0; i < l; i++)
+            cin >> strip[i];
+
+        for (i = 1; i < l; i++)
+        {
+            if (map[strip[i]] != map[strip[i - 1]])
+                ans++;
+        }
+
+        cout << ans + 1 << endl;
     }
 
     return 0;
