7488e87e76ed2f13876cd21cd2917f2da95ecf96
niharika2k00 <dniharika16@gmail.com>
2022-01-23 20:42:56 +0530

    codechef contest

---
+++ Palindromes_Not_Allowed.cpp
@@ -0,0 +1,35 @@
+/*
+________________________________________
+----------------------------------------
+ Author    :  Niharika Dutta
+ Code Link :  https://www.codechef.com/COOK137C/problems/NOPAL
+ Time Complexity :
+________________________________________
+----------------------------------------
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define loop1(n) for (ll i = 0; i < (n); i++)
+#define loop2(n) for (ll i = 1; i <= (n); i++)
+#define test  \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
